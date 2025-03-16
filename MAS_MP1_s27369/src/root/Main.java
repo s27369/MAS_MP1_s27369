3f25f841348ee6b49242749b32289d4abbf02ed2
s27369 <s27369@pjwstk.edu.pl>
2025-03-16 21:29:05 +0100

    base done, todo: add references between batalion and brygada, dowodca and zolnierz; add trwałość; add class method; add atr pochodny

--- MAS_MP1_s27369/src/root/Main.java
+++
@@ -1,8 +0,0 @@
-package root;
-
-public class Main {
-    public static void main(String[] args) {
-        Brygada b1 = new Brygada(18, "Stołeczna", "Warszawa");
-        System.out.println(b1);
-    }
-}
